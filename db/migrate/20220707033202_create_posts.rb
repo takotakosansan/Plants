66f855c6574e4b864d49c6f1b9da3c237e3ed91a
takotakosansan <tako.noboriya@icloud.com>
2022-07-07 03:41:17 +0000

    Postコントローラーとモデルの作成

---
+++ db/migrate/20220707033202_create_posts.rb
@@ -0,0 +1,9 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+      t.string :name,              null: false, default: ""
+      t.string :description,              null: false, default: ""
+      t.timestamps
+    end
+  end
+end
