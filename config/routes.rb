a42cbc4ace9520b2283286f03549cb4986e26a94
takotakosansan <tako.noboriya@icloud.com>
2022-07-10 23:51:58 +0000

    投稿機能$投稿画面&Top&About

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+root to: 'public/homes#top'
+get'about' => 'public/homes#about'
 # 顧客用
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
@@ -11,5 +14,14 @@ devise_for :customers,skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+scope module: :public do
+    resources :posts, only:[:new,:index,:show,:edit,:create]
+  end
+  
+namespace :admin do
+    root :to => 'homes#top'
+    resources :posts, only:[:index, :new, :create, :show, :edit, :update]
+    resources :customers, only:[:index, :show, :update, :edit]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
