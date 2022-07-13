a9fc46ec5bc9d512d73c52688f2dbde9921ee81d
takotakosansan <tako.noboriya@icloud.com>
2022-07-13 09:00:28 +0000

    画像表示

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -17,7 +17,8 @@ class Public::PostsController < ApplicationController
   end
   
   def show 
-    @post = Post.find(params[:id])  
+    @post = Post.find(params[:id])
+    @post_comment = PostComment.new
   end
   
   def edit
