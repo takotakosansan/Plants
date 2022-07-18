e08c686907e324f1c0b11ecff531d1f09fab1cd1
takotakosansan <tako.noboriya@icloud.com>
2022-07-18 11:52:22 +0000

    layout修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,6 +8,9 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
   
+  def index
+  @customer = Customer.all
+  end
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
