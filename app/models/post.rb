71cdaff61f81241134b37729bb60e2108b7420d4
takotakosansan <tako.noboriya@icloud.com>
2022-07-23 14:55:20 +0000

    post.rbの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -46,17 +46,17 @@ class Post < ApplicationRecord
    end
   end
 
-  # def self.looks(search, word)
-  #   if search == "perfect_match"
-  #     @post = Post.where("name LIKE?","#{word}")
-  #   elsif search == "forward_match"
-  #     @post = Post.where("name LIKE?","#{word}%")
-  #   elsif search == "backward_match"
-  #     @post = Post.where("name LIKE?","%#{word}")
-  #   elsif search == "partial_match"
-  #     @post = Post.where("name LIKE?","%#{word}%")
-  #   else
-  #     @post = Post.all
-  #   end
-  # end
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @post = Post.where("name LIKE?","#{word}")
+    elsif search == "forward_match"
+      @post = Post.where("name LIKE?","#{word}%")
+    elsif search == "backward_match"
+      @post = Post.where("name LIKE?","%#{word}")
+    elsif search == "partial_match"
+      @post = Post.where("name LIKE?","%#{word}%")
+    else
+      @post = Post.all
+    end
+  end
 end
