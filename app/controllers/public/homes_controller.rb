7594f2a63bcd84ae508cc623feddab8032b3a4b2
takotakosansan <tako.noboriya@icloud.com>
2022-07-25 07:39:36 +0000

    post_status index

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,7 +3,7 @@ class Public::HomesController < ApplicationController
     @posts = Post.all.order('created_at DESC').limit(4)
   end
   def new_guest
-      customer = Customer.find_or_create_by!(email: 'guest@example.com') do |customer|
+      customer = Customer.find_or_create_by!(email: 'guest2@example.com', name: 'guest') do |customer|
       customer.password = SecureRandom.urlsafe_base64
       # customer.confirmed_at = Time.now  # Confirmable を使用している場合は必要
   end
